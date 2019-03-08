aff89710ff6976b9424281a734456307f11697e1
Herman Tam <hermantam@hermans-mbp.lan>
2019-03-08 00:46:47 -0800

    Done adding IfForExample

--- src/IfForExamples.java
+++ src/IfForExamples.java
@@ -2,7 +2,7 @@ public class IfForExamples
 {
     public static void main(String[] args) // we will start with IF example
             // template if(condition) statement;
-    {
+   /* {
         int a = 2, b = 3, c;
 
         if (a<b) // if statement don't need semicolons because it is in a paramenter
@@ -24,6 +24,29 @@ public class IfForExamples
         System.out.println("c contains 1");
         if(c>=0) System.out.println("c is a non negative");
         if(c<0) System.out.println("c is negative\t\n");
+
+    }*/
+
+    /*{ // this will be a for loop
+        int count; //we will use count to have a better grasp of the concept
+        for(count =0; count <5; count++) // count will start at zero, and if its less then 5, it will add it self
+            //and repeat until it reaches the boolean statement count < 5
+            //count will stop once reaching 4
+        System.out.println("Count of: "+ count);
+
+        System.out.println("Finally!!");
+
+    }*/
+    { //This will be another if with blocks of codes
+        double a,b,c; //we will use the first two varbles for now;we will use double incase hou want to try decimal
+        a=2;
+        b=4;
+        if(a !=0)
+        {//This is the beginnig of the block; anything in this block will reflect on the if statement
+            System.out.println("a and zero are not as one!!!");
+            c = b /a ; //now we will use the third varible we didn't use yet
+            System.out.println("b divided a is "+ c);
+        }
 
     }
 }
